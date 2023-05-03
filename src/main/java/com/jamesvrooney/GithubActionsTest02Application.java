12535491cf819cf91b9d10aa04bfcf2519b4fb5f
jamesvrooney <jamesvrooney@hotmail.com>
2023-05-03 18:37:51 +0100

    Added Spring Boot project.

---
+++ src/main/java/com/jamesvrooney/GithubActionsTest02Application.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTest02Application.class, args);
+	}
+
+}
